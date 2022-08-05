770cda6056a1cbac88fd99ddfe37093505cca3ce
Alan Marini <pacopedacitow@gmail.com>
2022-08-04 21:01:25 -0300

    Generación de Archivos con spring

---
+++ src/main/java/com/portfolio/apb/ApbApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.apb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/apb/ApbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.apb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
