b868f0eb080da2564e2dd57ae44ae7d8554ac028
Alan Marini <pacopedacitow@gmail.com>
2022-08-06 00:26:39 -0300

    Se crearon los JavaPackage y JavaClass
    Se crearon las entidades, controladores, interface, repositorio y servicios. Cada uno con sus correspondientes JavaClass

---
+++ src/main/java/com/portfolio/apb/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.apb.Repository;
+
+import com.portfolio.apb.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long>{
+    
+}
