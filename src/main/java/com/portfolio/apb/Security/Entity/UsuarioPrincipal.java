2e3224741247a84a775921c8b214b40627b398ec
Alan Marini <pacopedacitow@gmail.com>
2022-08-16 13:39:55 -0300

    Update de Seguridad: Usuarios y Roles

---
+++ src/main/java/com/portfolio/apb/Security/Entity/UsuarioPrincipal.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.apb.Security.Entity;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+public class UsuarioPrincipal implements UserDetails{
+    
+}
